2c5f6c1d2ee3d1b6d987b67bc7f5072b7d01c86a
Rodrigo López Dato <rodrigo@auth0.com>
2015-10-07 01:56:17 -0300

    Improve failing test

--- test/fail.js
+++ test/fail.js
@@ -2,5 +2,6 @@ var dotenv = require('../index.js');
 var assert = require('assert');
 
 assert.throws(function () {
-  dotenv.load({sample: '.env.fail.sample'});
-});
+  dotenv.load({sample: '.env.fail'});
+  // Consider providing an Error class
+}, /Missing environment variables/);
