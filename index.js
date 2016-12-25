cebe645824bd8657a22550e0798f441d47e83fc8
Rodrigo López Dato <rolodato@rolodato.com>
2016-12-25 18:50:56 -0300

    Add custom error class, improve error message
    Closes #10

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 
 var dotenv = require('dotenv');
 var fs = require('fs');
+var MissingEnvVarsError = require('./MissingEnvVarsError.js');
 
 function difference (arrA, arrB) {
     return arrA.filter(function (a) {
@@ -24,13 +25,14 @@ module.exports = {
         options = options || {};
         options.silent = options.silent || !fs.existsSync('.env');
         dotenv.load(options);
-        var sampleVars = dotenv.parse(fs.readFileSync(options.sample || '.env.example'));
+        var sample = options.sample || '.env.example';
+        var sampleVars = dotenv.parse(fs.readFileSync(sample));
         var allowEmptyValues = options.allowEmptyValues || false;
         var processEnv = allowEmptyValues ? process.env : compact(process.env);
         var missing = difference(Object.keys(sampleVars), Object.keys(processEnv));
 
         if (missing.length > 0) {
-            throw new Error('Missing environment variables: ' + missing.join(', '));
+            throw new MissingEnvVarsError(allowEmptyValues, options.path || '.env', sample, missing);
         }
         return true;
     },
@@ -38,3 +40,4 @@ module.exports = {
 };
 
 module.exports.load = module.exports.config;
+module.exports.MissingEnvVarsError = MissingEnvVarsError;
