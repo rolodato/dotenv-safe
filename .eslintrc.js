6d5f58b3f63d03dbf127463dfe2c0e38be58dd82
Nikita Galkin <nikita@galk.in>
2017-01-03 08:23:23 +0200

    Fix inconsistent tests. Add eslint. Sort package.json

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+module.exports = {
+  "extends": "eslint:recommended",
+  "env": {
+    "node": true,
+    "mocha": true
+  },
+  "rules": {
+    "no-trailing-spaces": "error",
+    "semi": "error",
+    "indent": ["error", 4],
+    "comma-dangle": ["error", "never"]
+  }
+};
