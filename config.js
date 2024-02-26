e0d1c4ec86a563a1340dd4d1ee7839808b1b4b58
Rodrigo López Dato <rolodato@rolodato.com>
2024-02-26 12:24:05 -0300

    support DOTENV_CONFIG_ALLOW_EMPTY_VALUES

--- config.js
+++ config.js
@@ -5,6 +5,10 @@
         options.example = process.env.DOTENV_CONFIG_EXAMPLE;
     }
 
+    if (process.env.DOTENV_CONFIG_ALLOW_EMPTY_VALUES != null) {
+        options.allowEmptyValues = true;
+    }
+
     process.argv.forEach(function (val) {
         var matches = val.match(/^dotenv_config_(.+)=(.+)/);
         if (matches) {
